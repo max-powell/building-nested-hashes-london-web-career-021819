9c540241818f278705e789e25a0b0d43e2404860
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 11:35:23 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       hero_friends: {}
       },
     :capulet => {
-      #your key/value pairs here!
+      patriarch: {},
+      matriacrch: {},
+      hero: {},
+      hero_friends: {}
       }}
 
 
